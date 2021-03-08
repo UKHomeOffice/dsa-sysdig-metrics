516695f11e6b3e6d9e299825fab29ba2e25150a1
Charles <charles.poullet@digital.homeoffice.gov.uk>
2021-03-08 16:47:58 +0000

    Edited converttojson file to correctly remove irrelevant strings

--- scripts/converttojson.sh
+++ scripts/converttojson.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed 's/,$//' api.log | grep '{"proxy_proto_address"' | jq -s . > metrics.json #this needs to pick up rotated log
-sed 's/,$//' frontend.log | grep '{"proxy_proto_address"' | jq -s . > metrics_frontend.json
+sed 's/,$//' api.log | awk -v s='{"proxy_proto_address' 'index($0, s) == 1' | grep '\" }$' | jq -s . > metrics.json #this needs to pick up rotated log
+sed 's/,$//' frontend.log | awk -v s='{"proxy_proto_address' 'index($0, s) == 1' | grep '\" }$' | jq -s . > metrics_frontend.json
